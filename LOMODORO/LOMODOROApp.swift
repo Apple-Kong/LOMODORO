5081d80a129f83f5a02873d84290906154e775b6
Apple-Kong <kongthn@gmail.com>
2023-04-15 18:48:14 +0900

    Initial Commit

---
+++ LOMODORO/LOMODOROApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOMODOROApp.swift
+//  LOMODORO
+//
+//  Created by GOngTAE on 2023/04/15.
+//
+
+import SwiftUI
+
+@main
+struct LOMODOROApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
