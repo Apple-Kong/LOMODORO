3e615216c206fcc5a3330e61d1a6cb673a7d16d9
Apple-Kong <kongthn@gmail.com>
2023-04-15 19:31:20 +0900

    add Color extension

---
+++ LOMODORO/Color.swift
@@ -0,0 +1,28 @@
+//
+//  Color.swift
+//  LOMODORO
+//
+//  Created by GOngTAE on 2023/04/15.
+//
+
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        var rgbValue: UInt64 = 0
+
+        scanner.scanHexInt64(&rgbValue)
+
+        let red = Double((rgbValue & 0xFF0000) >> 16) / 255.0
+        let green = Double((rgbValue & 0x00FF00) >> 8) / 255.0
+        let blue = Double(rgbValue & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue)
+    }
+    
+    static let primary = Color(hex: "FF0000")
+    
+    static let tint = Color(hex: "FF0000")
+}
+
---
+++ LOMODORO/RoundedButton.swift
@@ -0,0 +1,20 @@
+//
+//  RoundedButton.swift
+//  LOMODORO
+//
+//  Created by GOngTAE on 2023/04/15.
+//
+
+import SwiftUI
+
+struct RoundedButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RoundedButton_Previews: PreviewProvider {
+    static var previews: some View {
+        RoundedButton()
+    }
+}
